97981b11678f615cdbf9ddf62a9ef8241b2f7476
Logi Haraldsson <logiharaldsson@Logis-MacBook-Pro.local>
2021-06-02 13:01:10 +0000

    broken our homepage into smaller components finished styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import HomePage from './HomePage.component';
+import HomePage from './pages/homepage/homePage.component';
 
 function App() {
   return (
---
+++ src/pages/homepage/homePage.component.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './homePage.styles.scss';
+
+import Directory from '../../components/directory/directory.component';
+
+const HomePage = () => {
+    return (
+        <div className='homepage'>
+            <Directory />
+        </div>
+    )
+}
+
+export default HomePage;
