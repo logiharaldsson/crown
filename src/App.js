eeda56f5edeb73d90d9be047ac317594b1e13e5a
Logi Haraldsson <logiharaldsson@Logis-MacBook-Pro.local>
2021-06-12 19:59:11 +0000

    Header component added. Navigation through link added to all pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+// Styles
 import './App.css';
 
+// Pages
 import HomePage from './pages/homepage/homePage.component';
 import ShopPage from './pages/shop/shop.component';
+// Components
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      {/* Header is placed outside of switch in order to stay on all pages */}
+      <Header />
+      {/* Switch is used to control the routes on pages and navigation */}
       <Switch>
         <Route exact path='/' component={HomePage}/>
         <Route path='/shop'  component={ShopPage}/>
