a52b4f65c7d4279bc515c57cfd6774643ef48da8
Logi Haraldsson <logiharaldsson@Logis-MacBook-Pro.local>
2021-06-26 12:48:31 +0000

    installed reselect library. Implemented cart.selectors to enhance performance on rendered components -> memoized selectors

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 import CartItem from '../cart-item/cart-item.component';
 
+import { selectCartItems } from '../../redux/cart/cart.selector';
+
 import CustomButton from '../custom-button/custom-button.component';
 import './cart-dropdown.styles.scss';
 
@@ -19,8 +21,8 @@ const CartDropdown = ({ cartItems }) => (
     </div>
 ); 
 
-const mapStateToProps = ({ cart: { cartItems } }) => ({
-    cartItems
-})
+const mapStateToProps = (state) => ({
+    cartItems: selectCartItems(state)
+});
 
 export default connect(mapStateToProps)(CartDropdown);
