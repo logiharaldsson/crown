4fc7dc55598454137a0969c71a42fad0474c2c65
Logi Haraldsson <logiharaldsson@Logis-MacBook-Pro.local>
2021-06-27 21:57:18 +0000

    installed redux-persist for localStorage control. Updated the directory and moved the state to a directory.reducer

--- src/index.js
+++ src/index.js
@@ -4,9 +4,10 @@ import ReactDOM from 'react-dom';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 
 // redux folder 
-import store from './redux/store';
+import {store, persistor } from './redux/store';
 
 // style and app
 import './index.css';
@@ -16,7 +17,9 @@ import App from './App';
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
-      <App />
+      <PersistGate persistor={persistor}>
+        <App />
+      </PersistGate>
     </BrowserRouter>
   </Provider>,
   
